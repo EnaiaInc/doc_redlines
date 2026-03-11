df57d342a66986b032cd33660393b97519f96931
Neil Berkman <neil@xuku.com>
2026-03-10 23:12:42 -0700

    Add rustler precompiled checksums

---
+++ checksum-Elixir.DocRedlines.Native.exs
@@ -0,0 +1,14 @@
+%{
+  "libdoc_redlines_nif-v0.5.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:9e2c9e7478bd22fee1fd905113be6ab79e945f92be3fef23d3d101302a7ea698",
+  "libdoc_redlines_nif-v0.5.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:4da1f1838bf46840dc36b7a33ac9c0ba9800256f265c443bb3e57c78e5742648",
+  "libdoc_redlines_nif-v0.5.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:92e10d890826313ad50e5104d56e42f40997ecb5d7a89acfc33bd9a8d3ab7add",
+  "libdoc_redlines_nif-v0.5.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:eb436df1e60d6bdbe08229363437498a4da8b5fa18d38367039f1d8ef0c80b38",
+  "libdoc_redlines_nif-v0.5.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:7d5115064c47bf7f815fc01962e02c10652ae36fbf534d137b86e49099796d47",
+  "libdoc_redlines_nif-v0.5.0-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:c3ee3564d13d3c6e45c62ccacb06ef80d86c52c6f2283bb7336a0f2fbda87b05",
+  "libdoc_redlines_nif-v0.5.0-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:4e76a64804db10d4383f7308aa57acd89c9ba95a5c8729d25562940577c0c53a",
+  "libdoc_redlines_nif-v0.5.0-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:eb3399330ba9c2316bd0f3f24405e445a6007d0c40f784a5eeb822e34f691845",
+  "libdoc_redlines_nif-v0.5.0-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:693a07176437ab29738c7f22d72d47739c1aeb8138ec3be0047756325a5115a1",
+  "libdoc_redlines_nif-v0.5.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:29aa3b80e078cf14d82bd51158ad0ecfb5250caf2c210553a89413fbee0cd7ed",
+  "libdoc_redlines_nif-v0.5.0-nif-2.17-x86_64-apple-darwin.so.tar.gz" => "sha256:1447f27d9a116d63c2c861f868d9a03c6efb8a2c7a08525fd5b1f3920f3bc3f0",
+  "libdoc_redlines_nif-v0.5.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:5432bf92f4c2f33691a9a963af960687ff285496ce6b4cbb3332f4c741afbdb6",
+}
